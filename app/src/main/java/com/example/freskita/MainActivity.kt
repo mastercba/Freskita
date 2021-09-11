5f9603054b85ff1017071aa0cd17050a5ca0cbc9
Marco Torrico <marcojtorrico@gmail.com>
2021-09-10 23:17:01 -0400

    menu-home : Main Menu

--- app/src/main/java/com/example/freskita/MainActivity.kt
+++ app/src/main/java/com/example/freskita/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.freskita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+
+    //<!-----AppBar Menu, Botones:produccion,compras,ventas & salir------>
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.produccion -> {
+                // 1. creo el Intent
+                //val go2Siembra = Intent(this, SiembraActivity::class.java)
+                // 2. Inicializar la actividad con el Intent creado
+                //this.startActivity(go2Siembra)
+            }
+            R.id.compras -> {
+                //getSiembra()
+            }
+            R.id.ventas -> {
+                //getSiembra()
+            }
+            R.id.salir -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    //------------------------------------------------------------------->
 }
